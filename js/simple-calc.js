1f8579cd2108821ba59a7e4f282fd79fb73815c7
shrfrn <shrfrn@gmail.com>
2025-11-03 18:00:38 +0200

    Add event handler placeholders

---
+++ js/simple-calc.js
@@ -0,0 +1,13 @@
+'use strict'
+
+function onNumClick(elCell) {
+    console.log(elCell.innerText)
+}
+
+function onOperatorClick(elCell) {
+    console.log(elCell.innerText)
+}
+
+function onCalc() {
+    console.log('Calculating')
+}
